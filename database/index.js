5950e6f4d13e845c7f8453ae1141079915ab5aae
ibrahim AKIN <ibrahim-akin@msn.com>
2020-10-09 23:21:08 +0300

    update

--- database/index.js
+++ database/index.js
@@ -10,15 +10,28 @@ const put = ({ name, age, count, createdDate }, callback) => {
         database: auth.database
     });
 
-    let sqlQuery = `INSERT INTO person (Name, Age, Count, CreatedDate) VALUES ('${name}', ${age}, ${count}, '${createdDate}');`;  //NOW() | ${createdDate}
+    let sqlInsertQuery = `INSERT INTO person (Name, Age, Count, CreatedDate) VALUES ('${name}', ${age}, ${count}, '${createdDate}');`;  //NOW() | ${createdDate}
     con.connect(function (err) {
         if (err) throw err;
-        con.query(sqlQuery, function (err, result) {   // Sorgu işletilir.
+        con.query(sqlInsertQuery, function (err, result) {   // Sorgu işletilir.
             if (err) { throw err; }
-            console.log("DB  Connected!");
-            callback();                                // Kullanıcıya döndürülür.
+            let { insertId } = result;                       // Veritabanında tutulduğu id
+            console.log("DB  Connected!", insertId);
+            callback();                                      // Kullanıcıya döndürülür.
+
+            //getData(insertId);                             // Eğer veritabanından çekilip döndürülecekse
         });
+
     });
+
+    const getData = (insertId) => {
+        let sqlSelectQuery = `SELECT * FROM person WHERE idPerson=${insertId}`;
+        con.query(sqlSelectQuery, (err, result) => {
+            if (err) { throw err; }
+            console.log(result[0]);
+            //callback(result[0]);
+        });
+    }
 }
 
 module.exports = put;
